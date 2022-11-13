7c318085da2faf7276e0efae14c28e307ccaeebc
IrvanSN <30742772+IrvanSN@users.noreply.github.com>
2022-11-13 10:11:11 +0700

    add dashboard

--- controller/home.js
+++ controller/home.js
@@ -1,7 +1,22 @@
-const moment = require('moment');
+const Form = require('../model/Form')
 
 module.exports = {
   index: async (req, res) => {
-    res.render('home/index', { title: 'Home' })
+    Form.aggregate([
+      { $unwind: "$isFulfilled" },
+      {
+        $group: {
+          _id: "$isFulfilled",
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { _id: 1 } },
+
+    ]).then(r => {
+      const fulfilled = r.find(x => x._id === true).count
+      const notFulfilled = r.find(x => x._id === false).count
+
+      res.render('home/index', { title: 'Home', fulfilled, notFulfilled, sum: fulfilled + notFulfilled })
+    })
   }
 }
